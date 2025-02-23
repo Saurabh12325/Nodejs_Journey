b9fe8bd0c4612b68d410ba4ed535a6f77e15acad
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-02-23 05:56:52 +0530

    understand express

--- 07ExpressJsIntroduction/index.js
+++ 07ExpressJsIntroduction/index.js
@@ -2,13 +2,29 @@ const express = require("express"); // Import Express
 const app = express(); // Create an Express application
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the home page"); // Send response
+
+    res.send(`Welcome to the home page 
+        <a href= "/about">go to about page</a>
+        `); // Send response
 });
 app.get("/about", (req, res) => {
-    res.send("Welcome to the about page"); // Send response
+    res.send(`
+        <h1>Welcome to the about page</h1>
+        <input type = 'text', placeholder = 'name user'value="${req.query.name}"  />
+        <button>click me </button>
+         <a href= "/">go to home page</a>
+        
+        `); // Send response
 });
 app.get("/course", (req, res) => {
-    res.send("Welcome to the course page"); // Send response
+    res.send(
+        [
+            {"name": "saurabh",
+                "course": "btech"},
+                {"name": "saurabh",
+                    "course": "btech"}
+        ]
+    ); // Send response
 });
 
 const PORT = 3000;
