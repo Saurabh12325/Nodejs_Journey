44641efdb0de96c8ad5677724e3a50b0288080f1
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-02-27 02:10:15 +0530

    changes

--- 16PlayWithApi/api.js
+++ 16PlayWithApi/api.js
@@ -1,5 +1,6 @@
 const express  =  require('express')
 const dbConnect = require('./mongodb')
+const mongodb = require('mongodb')
 const app = express()
 app.use(express.json())
 //get api 
@@ -14,16 +15,27 @@ app.get('/', async (req,res)=>{
 app.post('/', async (req,res)=>{
     const db  = await dbConnect();
     const result = await db.insertOne( req.body )
-      res.status(200).send(req.body)
+      res.status(200).send(result)
       if(result.acknowledge){
         console.log('Posted Successfully')
       }
 })
-app.listen(4000)
-//update api
-// app.put('/', async (req,res)=>{
-//    const db = await dbConnect();
-//    const result = await db.updateOne( req.body )
+
+// update api
+app.put('/:name', async (req,res)=>{
+   const data = await dbConnect();
+   const result = await data.updateOne(
+    {name:req.params.name},{$set:req.body}
+   )
+   res.status(200).send({result:'update'})
    
 
-// })
+})
+//Delete api
+app.delete('/:id',async (req,res)=>{
+  const data = await dbConnect();
+  const result = await data.deleteOne(
+    {_id: new mongodb.ObjectId(req.params.id)})
+res.send(result)
+})
+app.listen(4000)
