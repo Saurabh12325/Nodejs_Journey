b3ce353f638d79b03f71f07e00e62e0251d31189
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-11-30 02:51:30 +0530

    socketimplementation

--- 24Socket/server.js
+++ 24Socket/server.js
@@ -1,1 +1,13 @@
-  
+  const express = require('express')
+  const http = require('http')
+  const sockeIo = reuire('socket.io')
+
+
+  const app = express()
+  const server = http.createServer(app)
+
+  //initiate socket.io and attach this to the http server
+  const io = sockeIo(server)
+
+
+  app.use(express.static('public'))
